4664b38aa339cff7e5b503274de5ce443cd4d0b3
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-09-26 11:04:41 +0100

    Remove PyQt6 dependency
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- pyqt6/pyqt-synth/QtSynth.py
+++ pyqt6/pyqt-synth/QtSynth.py
@@ -12,40 +12,21 @@ import argparse
 # import serial
 import pyqtgraph as pg
 import numpy as np
-from scipy.io.wavfile import write
+# from scipy.io.wavfile import write
 # from scipy import signal
 from Signal import Signal, Sine, Square, Sawtooth, Chirp
 from datetime import datetime
+from pyqtgraph.Qt import (
+        QtWidgets,
+        QtCore,)
 
-from PyQt6.QtCore import (
-        QSize,
-        QTimer,
-        Qt,
-        )
-
-from PyQt6.QtWidgets import (
-    # QButtonGroup,
-    QWidget,
-    QApplication,
-    QDial,
-    QLineEdit, QTableView,
-    QMainWindow,
-    QVBoxLayout,
-    QHBoxLayout,
-    QGridLayout,
-    QDialog,
-    # QDialogButtonBox, QCheckBox, QComboBox, QGroupBox, QMessageBox,
-    QPushButton,
-    QSlider,
-    # QSpinBox, QRadioButton, QTabWidget,
-    QLabel,
-)
 
 UPDATE_PERIOD = 1000  # Sample Period in millisec
 NUM_HARMONICS = 7
 INITIAL_VOL = np.array([10, 5, 0, 0, 0, 1, 0])
 AUDIO_RATE = 44100
 freq = 440
+FREQ_SIGNAL = 440
 length = 0.02  # in sec
 num_samples = int(AUDIO_RATE * length)
 
@@ -73,59 +54,47 @@ def parseCommandLine():
     return args
 
 
-class HarmonicControl(QWidget):
+class HarmonicControl(QtWidgets.QWidget):
     def __init__(self, parent=None, order=5):
         super().__init__(parent)
         # super().__init__(*args, **kwargs)
         # print(f'order = {order}')
-
-        layout = QVBoxLayout()
-        lblOrd = QLabel(f'Ordem = {order}')
+        self.order = order
+        layout = QtWidgets.QVBoxLayout()
+        lblOrd = QtWidgets.QLabel(f'Ordem = {order}')
         layout.addWidget(lblOrd)
         # self.slider = QSlider(Qt.Orientation.Vertical, self)
-        self.slider = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal, self)
         self.slider.setGeometry(0, 0, 20, 50)
         self.slider.setMinimum(0)
         self.slider.setMaximum(180)
-        self.slider.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.slider.setTickPosition(QtWidgets.QSlider.TickPosition.TicksBelow)
         self.slider.setTickInterval(90)
         layout.addWidget(self.slider)
-        layout.addWidget(QLabel('Phase'))
+        layout.addWidget(QtWidgets.QLabel('Phase'))
 
-        self.dialAmp = QDial()
-        self.dialAmp.setGeometry(0, 0, 30, 30)
-        self.dialAmp.setMinimum(0)
-        self.dialAmp.setMaximum(10)
-        self.dialAmp.setValue(0)
+        dial = QtWidgets.QDial()
+        dial.setGeometry(0, 0, 30, 30)
+        dial.setMinimum(0)
+        dial.setMaximum(10)
+        # self.dialAmp.setValue(0)
+        dial.setNotchesVisible(True)
+        self.dialAmp = dial
         self.dialAmp.valueChanged.connect(self.sliderMoved)
         layout.addWidget(self.dialAmp)
-        layout.addWidget(QLabel('Ampl'))
+        layout.addWidget(QtWidgets.QLabel('Ampl'))
         self.setLayout(layout)
 
     def sliderMoved(self):
-        print("Dial value = %i" % (self.dialAmp.value()))
+        print(f"Dial value = %i" % (self.dialAmp.value()))
 
 
-class EchoText(QWidget):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.layout = QGridLayout()
-        self.setLayout(self.layout)
-
-        self.textbox = QLineEdit()
-        self.echo_label = QLabel('')
-
-        self.textbox.textChanged.connect(self.textbox_text_changed)
-
-        self.layout.addWidget(self.textbox, 0, 0)
-        self.layout.addWidget(self.echo_label, 1, 0)
 
     def textbox_text_changed(self):
         self.echo_label.setText(self.textbox.text())
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Fourier Shyntesizer")
@@ -133,11 +102,11 @@ class MainWindow(QMainWindow):
         self.simul = args['simUl']
         self.val = 5.0
 
-        container = QWidget()
-        layoutMain = QVBoxLayout()
-        layoutHarmonics = QHBoxLayout()
+        container = QtWidgets.QWidget()
+        layoutMain = QtWidgets.QVBoxLayout()
+        layoutHarmonics = QtWidgets.QHBoxLayout()
         layoutMain.addLayout(layoutHarmonics)
-        lblTr = QLabel('Temperature RED')
+        lblTr = QtWidgets.QLabel('Temperature RED')
         lblTr.setStyleSheet("color: red")
         lblTr.setFixedHeight(80)
         # layoutMain.addWidget(lblTr, 0, 0)
@@ -174,14 +143,14 @@ class MainWindow(QMainWindow):
         # plotT.setYRange(10, 105)
         # plotT.plot(self.TredArray)  # pen=({'color: red', 'width: 1'}), name="ch{1}")
 
-        layoutButtons = QHBoxLayout()
-        saveBtn = QPushButton('Save Data')
+        layoutButtons = QtWidgets.QHBoxLayout()
+        saveBtn = QtWidgets.QPushButton('Save Data')
         saveBtn.clicked.connect(self.save_data)
         layoutButtons.addWidget(saveBtn)  # , 3, 0)
-        playBtn = QPushButton('Play Sound')
+        playBtn = QtWidgets.QPushButton('Play Sound')
         playBtn.clicked.connect(self.pydub_play)
         layoutButtons.addWidget(playBtn)  # , 3, 0)
-        saveWav = QPushButton('Save Wav')
+        saveWav = QtWidgets.QPushButton('Save Wav')
         saveWav.clicked.connect(self.save_wav)
         layoutButtons.addWidget(saveWav)  # , 3, 0)
         layoutMain.addLayout(layoutButtons)
@@ -189,10 +158,10 @@ class MainWindow(QMainWindow):
         # layoutMain.addWidget(self.echotext_widget)
 
         container.setLayout(layoutMain)
-        self.setMinimumSize(QSize(1200, 700))
+        self.setMinimumSize(QtCore.QSize(1200, 700))
         self.setCentralWidget(container)
 
-        self.timer = QTimer()
+        self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_panels)
         self.timer.start(UPDATE_PERIOD)  # Sample Period in millisec
 
@@ -211,6 +180,14 @@ class MainWindow(QMainWindow):
     def update_panels(self):
         # xdata.append((float(data[0]) - timeStart )/1000.0 )
         print(self.harmonics[0].dialAmp.value())
+
+        sig = self.harmonics[0].dialAmp.value() * Sine(FREQ_SIGNAL, length=2)
+        for i in range(1, NUM_HARMONICS):
+            a = self.harmonics[i].dialAmp.value()
+            n = self.harmonics[i].order
+            siga = a * Sine(n * FREQ_SIGNAL, length=2)
+            sig = sig.__add__(siga)
+        self.sig = sig
         try:
             # self.SoundCurve.setData(self.time, self.y)
             self.SoundCurve.setData(self.sig.ts[:1024],
@@ -221,9 +198,25 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
+"""
+class EchoText(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+
+        self.textbox = QLineEdit()
+        self.echo_label = QLabel('')
+
+        self.textbox.textChanged.connect(self.textbox_text_changed)
+
+        self.layout.addWidget(self.textbox, 0, 0)
+        self.layout.addWidget(self.echo_label, 1, 0)
+"""
 # vim: syntax=python ts=4 sw=4 sts=4 sr et
